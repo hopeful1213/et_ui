090f6c159a34c735c1a08254476b8c375d0a126e
superuser <superuser@10.17.19.103>
2017-04-07 17:58:50 +0900

    sdfsf

--- etherAccountBook/dist/js/accData.js
+++ etherAccountBook/dist/js/accData.js
@@ -1,5 +1,47 @@
 
 
+
+var AccountValue = function(accountDate, balance){
+	this.accountDate = accountDate;
+	this.balance = balance;
+	this.pushMyAccount = function(myAccount){
+		this.myAccount += "|" + myAccount;
+	}
+	this.isInput = function(acc){
+		if((this.myAccount).indexOf(acc) != -1){
+			return true
+		}
+		return false;
+	}
+	this.caculateBalance= function(acc, value){
+		if(this.isInput(acc)){
+			this.balance += value;
+		}
+		else{
+			this.balance -= value;
+		}
+	}
+	this.getBalance = function(){
+		return this.balance;
+	}
+	
+} 
+
+var MakeSmallTable = function(idvalue, groupTime){
+	this.idvalue = idvalue;
+	this.groupTime = groupTime;
+	this.header = '<div id ="'  + this.idvalue + '888" data-toggle="collapse" class="box-body">'
+	+'<table class="table table-bordered table-hover"><tbody>';
+	this.footer = '</tbody></table></div>';
+	this.pushTr = function (time, isInput, evalue, egas, egasprice){
+		this.bodyValue += '<tr><td>'+ time + '</td>' + '<td>'
+				+isInput+'</td><td>' + evalue + '</td><td>' +egas + '</td><td>' + egasprice + '</td></tr>';
+	}
+	this.getSmallTable = function(){
+		return this.header + this.bodyValue + this.footer; 
+	}
+}
+
  // var flickerAPI = "http://api.flickr.com/services/feeds/photos_public.gne?jsoncallback=?";
  //var getAccountDataUrl = "http://api.etherscan.io/api?module=account&action=txlist&startblock=0&endblock=99999999&sort=asc&apikey=VARZVFXFYBAKM1C5TYKSXRZ18GD6W7B6M9&address=?";
  //var Inputdata = "0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a";
@@ -16,6 +58,10 @@ $(document).ready(function(){
 	var month = months[a.getMonth()];
 	console.log("11111111111111111111111111111111111111111" + year + "년" + month);
 	$('#monthValue').append( month+ " " + year);
+	var aaa = new AccountValue('20170116',1000);
+	aaa.pushMyAccount('121212');
+	aaa.caculateBalance('12121222', 2000);
+	alert(aaa.getBalance());
 
 });
 
@@ -38,6 +84,7 @@ $.ajax({
 	var groupTime = "";
 	var totalEther 	= 0;
 	var totalGas 	= 0;
+	var getSmall;
 	$.each(msg.result, function(i, item){
 
 		//console.log(i + " :: rrr :: " + item.blockNumber);
@@ -45,36 +92,31 @@ $.ajax({
 		var timestamp2 = item.timeStamp * 1000;
 		var a = new Date(timestamp2);
 		var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-		var year = a.getFullYear();
+		var year = a.getFullYear(); 
 		var month = months[a.getMonth()];
 		var date = a.getDate();
 		var hour = a.getHours();
-		var min = a.getMinutes();
+		var min = a.getMinutes();0
 		var sec = a.getSeconds();
 		var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
+
 		console.log('time is ' + time);
-		if(groupTime != date  + month +  year){
+		if(groupTime != date  + month +  year){						
+			if(getSmall != null){
+				$('#'+ groupTime).append(getSmall.getSmallTable());
+			}			
 			groupTime = date  + month  +year;
+			getSmall = new MakeSmallTable(groupTime,groupTime);
             $('#example21').append(
-              '<tbody id="'+groupTime+'" data-toggle="collapse" data-target="#' + groupTime + '888">	'
-              + '<tr><td>'+ groupTime + '</td><td>'+((totalEther<0) ? '<font color = "blue">적자</font>' : '<font color = "red">이익</font>')
-              +'</td><td>' + totalEther + '</td><td>'  
-			  + totalGas + '</td><td>' + egasprice + '</td></tr></tbody>');
+              '<tbody id="'+'a' + groupTime+'" data-toggle="collapse" data-target="#' + groupTime + '888">	'
+              + '<tr><td>'+ groupTime + '</td><td><b>'+((totalEther<0) ? '<font color = "blue">적자</font>' : '<font color = "red">이익</font>')
+              +'</b></td><td>' + totalEther + '</td><td>'  
+			  + totalGas + '</td><td>' + egasprice + '</td></tr></tbody>');    		
     		totalEther 	= 0;
     		totalGas 	= 0;
 		     
 		}
-		//groupTime = date + month + year;
-		//data-toggle="collapse" data-target="#
 
-//		
-//		console.log(i + " :: rrr :: " + time);
-//		console.log(i + " :: rrr :: " + item.from);
-//		console.log(i + " :: rrr :: " + item.to);
-//		console.log(i + " :: rrr :: " + item.value);
-//		console.log(i + " :: rrr :: " + item.gas);
-//		console.log(i + " :: rrr :: " + item.gasPrice);
-//		console.log("---------------------");
 		var evalue = 0;
 		var egas = 0;
 		var egasprice = 0;
@@ -98,8 +140,12 @@ $.ajax({
 			totalGas += egas;
 		}
 		
-		$('"#'+groupTime +'"').append('<tr id ="'  + groupTime + '888"><td>'+ time + '</td>' + '<td>'
-				+isInput+'</td><td>' + evalue + '</td><td>' +egas + '</td><td>' + egasprice + '</td></tr>');
+		//$('#'+'a' + groupTime +'').append('<tr id ="'  + groupTime + '888" data-toggle="collapse"><td>'+ time + '</td>' + '<td>'
+		//		+isInput+'</td><td>' + evalue + '</td><td>' +egas + '</td><td>' + egasprice + '</td></tr>');		
+//		$('#'+'a' + groupTime +'').append('<tr><td>'+ time + '</td>' + '<td>'
+//				+isInput+'</td><td>' + evalue + '</td><td>' +egas + '</td><td>' + egasprice + '</td></tr>');
+		getSmall.pushTr(time, isInput, evalue, egas, egasprice);
+
 	});
  
 });  
@@ -119,3 +165,4 @@ $.getJSON("http://api.etherscan.io/api?module=account&action=txlist&startblock=0
         });   
  });  
 */
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
