127bdc45b86c8d15e1258bc922530111e327f74e
superuser <superuser@10.17.19.103>
2017-04-11 19:35:25 +0900

    ...

--- etherAccountBook/dist/js/accData.js
+++ etherAccountBook/dist/js/accData.js
@@ -1,9 +1,24 @@
 
 
 
-var AccountValue = function(accountDate, balance){
-	this.accountDate = accountDate;
+var AccountValue = function(account, balance){
+	this.myAccount = account;
+	this.accDate = [{value: '20171111', sum: 2}];
+	this.accDate2 = {value:'20171111', sum:2};
 	this.balance = balance;
+	var inventory = [
+	    {name: 'apples', quantity: 2},
+	    {name: 'bananas', quantity: 0},
+	    {name: 'cherries', quantity: 5}
+	];
+
+	this.lastPushDate = '';
+//	var ii = 'cherries';
+//	function findCherries(fruit) { 
+//	    return fruit.name === ii;
+//	}
+//	console.log(inventory.find(findCherries));
+	
 	this.pushMyAccount = function(myAccount){
 		this.myAccount += "|" + myAccount;
 	}
@@ -13,26 +28,59 @@ var AccountValue = function(accountDate, balance){
 		}
 		return false;
 	}
-	this.caculateBalance= function(acc, value){
+	this.caculateBalance= function(date, acc, value){
+		// 총 합계 구하기
 		if(this.isInput(acc)){
 			this.balance += value;
 		}
 		else{
 			this.balance -= value;
 		}
+
+		// 일자변 합계 구하기
+		if(this.lastPushDate == ''){
+			this.lastPushDate = date;
+		}
+		else if(this.lastPushDate < date){
+			this.lastPushDate = date;
+			// 신규생성 후 계산
+		}
+		else{
+			// 기존에 있다면 계산
+		}
+
+		//var 
+		//this.accData.forEach(function findit('2017111',index){this[index].value == });
+		//alert(this.accDate.find(findDate('20171111',this.accDate.length,this.accDate)));
+		//if(accDate.find())
+		//this.accDate.push('111');
+		//alert(this.accDate);
+		//this.accDate.push('111');
+		//alert(this.accDate);
 	}
 	this.getBalance = function(){
 		return this.balance;
 	}
-	
 } 
 
+var findDate = function(element, index, array) { 
+	alert('QQQQ' + index +'||| '+array);
+	alert(array[index].value + '  ' + element);
+	array.forEach()
+	  while (start <= Math.sqrt(element)) {
+		    if (element % start++ < 1) {
+		      return false;
+		    }
+		  }
+    return array[index].value === value;
+}
 var MakeSmallTable = function(idvalue, groupTime){
 	this.idvalue = idvalue;
 	this.groupTime = groupTime;
-	this.header = '<div id ="'  + this.idvalue + '888" data-toggle="collapse" class="box-body">'
-	+'<table class="table table-bordered table-hover"><tbody>';
-	this.footer = '</tbody></table></div>';
+	this.bodyValue ="";
+	this.header = '<td colspan="5" style="padding:0 0 0 0"><div id ="'  + this.idvalue + '888"  class="box-body collapse">'
+	+'<table class="table table-bordered table-hover "><tbody>';
+	this.footer = '</tbody></table></div></td>';
 	this.pushTr = function (time, isInput, evalue, egas, egasprice){
 		this.bodyValue += '<tr><td>'+ time + '</td>' + '<td>'
 				+isInput+'</td><td>' + evalue + '</td><td>' +egas + '</td><td>' + egasprice + '</td></tr>';
@@ -58,11 +106,8 @@ $(document).ready(function(){
 	var month = months[a.getMonth()];
 	console.log("11111111111111111111111111111111111111111" + year + "년" + month);
 	$('#monthValue').append( month+ " " + year);
-	var aaa = new AccountValue('20170116',1000);
-	aaa.pushMyAccount('121212');
-	aaa.caculateBalance('12121222', 2000);
-	alert(aaa.getBalance());
-
+	
+	//alert(aaa.getBalance());
 });
 
 
@@ -85,6 +130,10 @@ $.ajax({
 	var totalEther 	= 0;
 	var totalGas 	= 0;
 	var getSmall;
+	
+	var aaa = new AccountValue('20170116',0);
+	aaa.pushMyAccount('0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a');
+
 	$.each(msg.result, function(i, item){
 
 		//console.log(i + " :: rrr :: " + item.blockNumber);
@@ -101,18 +150,24 @@ $.ajax({
 		var time = date + ' ' + month + ' ' + year + ' ' + hour + ':' + min + ':' + sec ;
 
 		console.log('time is ' + time);
+		aaa.caculateBalance(item.from, evalue);
 		if(groupTime != date  + month +  year){						
 			if(getSmall != null){
 				//alert(getSmall.getSmallTable());
 				$('#'+ groupTime).append(getSmall.getSmallTable());
 			}			
-			groupTime = date  + month  +year;
+			groupTime = date  +'-'+ month+'-'  +year;
 			getSmall = new MakeSmallTable(groupTime,groupTime);
             $('#example21').append(
-              '<tbody id="' + groupTime+'" data-toggle="collapse" data-target="#' + groupTime + '888">	'
-              + '<tr><td>'+ groupTime + '</td><td><b>'+((totalEther<0) ? '<font color = "blue">적자</font>' : '<font color = "red">이익</font>')
-              +'</b></td><td>' + totalEther + '</td><td>'  
-			  + totalGas + '</td><td>' + egasprice + '</td></tr></tbody>');    		
+              //'<tbody>	' +
+      //        '<tr data-toggle="collapse" data-target="#' + groupTime + '888">'
+      //        +'<td>'+ groupTime + '</td><td><b>'+((totalEther<0) ? '<font color = "blue">적자</font>' : '<font color = "red">이익</font>')
+      //        +'</b></td><td>' + totalEther + '</td><td>'  
+		//	  + totalGas + '</td><td>' + egasprice + '</td></tr><tr id="' + groupTime+'"></tr>'); //</tbody>');  
+            '<tr data-toggle="collapse" data-target="#' + groupTime + '888">'
+            +'<td>'+ groupTime + '</td><td><b>'+aaa.getBalance()
+            +'</b></td><td>' + totalEther + '</td><td>'  
+			  + totalGas + '</td><td>' + egasprice + '</td></tr><tr id="' + groupTime+'"></tr>'); //</tbody>'); 
     		totalEther 	= 0;
     		totalGas 	= 0;
 		     
@@ -131,6 +186,7 @@ $.ajax({
 		if(item.gasPrice != 0){
 			egasprice = item.gasPrice / 1000000000000000000;
 		}
+
 		if('0xddbd2b932c763ba5b1b7ae3b362eac3e8d40121a' == item.from){
 			isInput = '<font color="blue">출금</font>';
 			totalEther -= evalue;
